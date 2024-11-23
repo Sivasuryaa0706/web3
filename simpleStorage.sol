307f50269f39e42197c160b8be8c2076557ae9cd
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-11-23 18:42:55 +0530

    Created Storing and retrieving functions

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.18;
 
 contract simpleStorage{
-    uint256 favNum = 2;
+    uint256 public favNum = 2;
+
+    function store(uint256 _favNum) public {
+        favNum = _favNum;
+    }
+
+    //View: read from the blockchain but cannot modify the contract's state
+    //pure: cannot read from the blockchain or modify the contract's state 
+    function retrieve() public view returns(uint256){
+        return favNum;
+    }
 }
