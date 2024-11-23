f6b12c3e08ae56aa229d370f88b9d2608e7e2699
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-11-23 18:09:04 +0530

    Basic Datatypes in solidity

---
+++ datatypes.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract datatyes{
+    bool hasFavNum = true;
+    uint256 favNum = 2; //Only +ve
+    int256 negFavNum = -2; //Both +ve,-ve
+    string favNumInText = "Two";
+    address myAddress = 0x66a1c2a9AB58E78c9823479862Ca40d65F00b639; //wallet address
+    bytes32 favBytes = "2";
+}
