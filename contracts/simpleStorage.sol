e8b0489fbc56ac6d4c510f35c6fa27cde4886f66
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-11-26 01:08:37 +0530

    Deploying one contract from another

---
+++ contracts/simpleStorage.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract simpleStorage{
+    uint256 public myFavNum;
+
+    //Struct
+    struct Person{
+        string name;
+        uint256 favNum;
+    }
+
+    //Dynamic array: no limit
+    Person[] public listOfPeoples;
+
+    //Declaring mapping. (key => value)
+    mapping(string => uint256) public nameToNum;
+    
+    function store(uint256 _myFavNum) public {
+        myFavNum = _myFavNum;
+    }
+
+    //View: read from the blockchain but cannot modify the contract's state
+    //pure: cannot read from the blockchain or modify the contract's state 
+    function retrieve() public view returns(uint256){
+        return myFavNum;
+    }
+
+    function addPerson(string memory _name,uint256 _num) public{
+        listOfPeoples.push(Person(_name,_num));
+        nameToNum[_name] = _num;
+    }
+}
---
+++ contracts/storageFactory.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+import "contracts/simpleStorage.sol";
+
+contract storageFactory{
+    
+    //Type Visibility Name (TVN)
+    simpleStorage public mySimpleStorage;
+
+    function createSimpleStorageContract() public{
+        mySimpleStorage = new simpleStorage(); //Simple storage contract has been deployed.
+    }
+}
