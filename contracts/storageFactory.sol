531a2e1578b03e6f5b58819dc4415e3a74e83655
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-11-26 01:12:34 +0530

    Improved imports

--- contracts/storageFactory.sol
+++ contracts/storageFactory.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
-import "contracts/simpleStorage.sol";
+import {simpleStorage} from "contracts/simpleStorage.sol";
 
 contract storageFactory{
     
