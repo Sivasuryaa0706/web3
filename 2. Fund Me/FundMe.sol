1ce157b79743271c7523597f5b08bde2f1b9519f
Sivasuryaa0706 <sivasuryaa2004@gmail.com>
2024-11-26 21:05:36 +0530

    Interfaces to get ETH/USD prices real-time

--- 2. Fund Me/FundMe.sol
+++ 2. Fund Me/FundMe.sol
@@ -2,11 +2,44 @@
 
 pragma solidity ^0.8.18;
 
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe{
+    uint256 public minUSD = 5;
+
     function fund() public payable {
         //Allow users to send money and minimum send money
+
         require(msg.value > 1e18, "Didn't send enough ETH"); //minimum 1e18 = 1ETH
     }
     
+    function getPrice() public{
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+
+    }
+
+    function getConversionRate() public{
+
+    }
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
+    }
     // function withdraw() public{}
 }
