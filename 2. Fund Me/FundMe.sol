05d77aaa36b8e964c99a220c949999b8d7929bf6
Sivasuryaa0706 <sivasuryaa2004@gmail.com>
2024-11-26 21:41:44 +0530

    Conversion of ETH to USD

--- 2. Fund Me/FundMe.sol
+++ 2. Fund Me/FundMe.sol
@@ -7,22 +7,26 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 
 contract FundMe{
-    uint256 public minUSD = 5;
+    uint256 public minUSD = 5e18;
 
     function fund() public payable {
         //Allow users to send money and minimum send money
 
-        require(msg.value >= 1e18, "Didn't send enough ETH"); //minimum 1e18 = 1ETH
+        require(getConversionRate(msg.value) >= 1e18, "Didn't send enough ETH"); //minimum 1e18 = 1ETH
     }
     
-    function getPrice() public{
+    function getPrice() public view returns(uint256){
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         //ABI
-
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int answer, , ,) = priceFeed.latestRoundData();
+        return uint256(answer*1e10);  //returns ETH Value in USD
     }
 
-    function getConversionRate() public{
-
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
+        return ethAmountInUsd;
     }
 
     function getVersion() public view returns (uint256) {
