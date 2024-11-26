1a1cd925afa183b70576b0c83935d9f9305d0019
Sivasuryaa0706 <sivasuryaa2004@gmail.com>
2024-11-26 22:23:01 +0530

    Created Price Converter Library

--- 2. Fund Me/FundMe.sol
+++ 2. Fund Me/FundMe.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.18;
 
-//Importing the interface directly from github
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;  //Saying that uint256 variables can be accessed by priceConverter
+
     uint256 public minUSD = 5e18;
     
     //To store all the transactions
@@ -16,27 +16,12 @@ contract FundMe{
     function fund() public payable {
         //Allow users to send money and minimum send money
 
-        require(getConversionRate(msg.value) >= 1e18, "Didn't send enough ETH"); //minimum 1e18 = 1ETH
+        //msg.value.getConversionRate() parameter followed by function when using library
+        require(msg.value.getConversionRate() >= minUSD, "Didn't send enough ETH"); //minimum 1e18 = 1ETH
         funders.push(msg.sender);
         addressToAmount[msg.sender] += msg.value;
     }
     
-    function getPrice() public view returns(uint256){
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int answer, , ,) = priceFeed.latestRoundData();
-        return uint256(answer*1e10);  //returns ETH Value in USD
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
-    }
+    
     // function withdraw() public{}
 }
---
+++ 2. Fund Me/PriceConverter.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+//Importing the interface directly from github
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+    function getPrice() public view returns(uint256){
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int answer, , ,) = priceFeed.latestRoundData();
+        return uint256(answer*1e10);  //returns ETH Value in USD
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
+    }
+}
