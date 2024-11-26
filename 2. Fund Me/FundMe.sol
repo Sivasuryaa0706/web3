4453cc3502fa4cf5836e1b75cc4f0e9ee3cc313d
Sivasuryaa0706 <sivasuryaa2004@gmail.com>
2024-11-26 21:49:07 +0530

    Storing all transactions of funders

--- 2. Fund Me/FundMe.sol
+++ 2. Fund Me/FundMe.sol
@@ -8,11 +8,17 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe{
     uint256 public minUSD = 5e18;
+    
+    //To store all the transactions
+    address[] public funders;
+    mapping(address funder => uint256 amount) public addressToAmount;
 
     function fund() public payable {
         //Allow users to send money and minimum send money
 
         require(getConversionRate(msg.value) >= 1e18, "Didn't send enough ETH"); //minimum 1e18 = 1ETH
+        funders.push(msg.sender);
+        addressToAmount[msg.sender] += msg.value;
     }
     
     function getPrice() public view returns(uint256){
