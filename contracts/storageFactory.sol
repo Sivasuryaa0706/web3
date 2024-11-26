a1085650616325017440d172c9105a7588615c86
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-11-26 12:20:17 +0530

    ABI

--- contracts/storageFactory.sol
+++ contracts/storageFactory.sol
@@ -6,9 +6,21 @@ import {simpleStorage} from "contracts/simpleStorage.sol";
 contract storageFactory{
     
     //Type Visibility Name (TVN)
-    simpleStorage public mySimpleStorage;
+    simpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public{
-        mySimpleStorage = new simpleStorage(); //Simple storage contract has been deployed.
+        simpleStorage mySimpleStorage = new simpleStorage(); //Simple storage contract has been deployed.
+        listOfSimpleStorageContracts.push(mySimpleStorage);
+    }
+
+    function sfStore(uint256 _ind,uint256 _num) public{
+        //To interact with another contract we need address, ABI
+        simpleStorage mySimpleStorage = listOfSimpleStorageContracts[_ind];
+        mySimpleStorage.store(_num);
+    }
+
+    function sfGet(uint256 _ind) public view returns(uint256){
+        simpleStorage mySimpleStorage = listOfSimpleStorageContracts[_ind];
+        return mySimpleStorage.retrieve();
     }
 }
